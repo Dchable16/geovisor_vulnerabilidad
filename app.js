f6f73df92b7885dcc4ef74394e5ee353ea558051
Dchable16 <dcm.chable89@gmail.com>
2025-09-25 17:25:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             styles: {
                 muted: { fillColor: '#A9A9A9', weight: 1, color: '#A9A9A9', fillOpacity: 0.2 },
-                selection: { color: '#00FFFF', weight: 2.5, opacity: .8 },
-                hover: { weight: 3, color: '#000', dashArray: '', fillOpacity: 0.7 }
+                selection: { color: '#00FFFF', weight: 2, opacity: .8 },
+                hover: { weight: 3, color: '#000', dashArray: '', fillOpacity: 0.95 }
             }
         },
 
